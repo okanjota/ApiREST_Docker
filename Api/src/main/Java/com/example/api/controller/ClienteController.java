4bd41e7663d3b4e1fbbd7b7456a823dcb62f59b5
okanjota <jaimeejunioorr@gmail.com>
2025-06-06 16:14:53 -0300

    Atualização

--- Api/src/main/Java/com/example/api/controller/ClienteController.java
+++ Api/src/main/Java/com/example/api/controller/ClienteController.java
@@ -12,13 +12,11 @@ import com.example.api.model.Cliente;
 @RequestMapping("/api/clientes")
 public class ClienteController {
     private List<Cliente> clientes = new ArrayList<>();
-    private AtomicLong contadorId = new AtomicLong(1); // Gera ID automático
-
     public ClienteController() {
-        clientes.add(new Cliente(contadorId.getAndIncrement(), "Leonardo Dias", "27 420956489", "leonardo@email.com"));
-        clientes.add(new Cliente(contadorId.getAndIncrement(), "Amara Hoffmann", "11 428429842", "amara@email.com"));
-        clientes.add(new Cliente(contadorId.getAndIncrement(), "Armando Ferrari", "27 669334200", "armando@gmail.com"));
-        clientes.add(new Cliente(contadorId.getAndIncrement(), "Juscelino Kubitschek", "11 489842948", "juscelino@gmail.com"));
+    clientes.add(new Cliente(4382L, "Leonardo Dias", "27 420956489", "leonardo@email.com"));
+    clientes.add(new Cliente(7251L, "Amara Hoffmann", "11 428429842", "amara@email.com"));
+    clientes.add(new Cliente(3197L, "Armando Ferrari", "27 669334200", "armando@gmail.com"));
+    clientes.add(new Cliente(8924L, "Juscelino Kubitschek", "11 489842948", "juscelino@gmail.com"));
     }
 
     @GetMapping
--- Dockerfile
+++ Dockerfile
@@ -2,5 +2,5 @@ FROM openjdk:17-zulu
 RUN mkdir/app
 WORKDIR /app 
 COPY target/*.jar /app/app/.jar
+COPY src ./app
 CMD ["java", "-jar","/app/app.jar"]
-
